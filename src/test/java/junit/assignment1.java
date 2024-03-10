9bb6e6d8f4d55464703b9a654f7801e95436e667
hwazenhamdan <hwazen.7@hotmail.com>
2024-03-10 16:13:16 +0300

    first project

--- src/test/java/junit/assignment1.java
+++ src/test/java/junit/assignment1.java
@@ -2,6 +2,6 @@ package junit;
 
 public class assignment1 {
     public static void main(String[] args) {
-        System.out.println("Hello in Assigment Project");
+        System.out.println("Hwazen");
     }
 }
